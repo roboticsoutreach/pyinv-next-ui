485f0da2493dc9aae9b0bd01b94b331ccef1b55d
Alex Colville <56324439+Alexbruvv@users.noreply.github.com>
2023-09-29 11:47:10 +0100

    chore: rename module directory to lib

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
-import { authOptions } from "@/module/authOptions";
+import { authOptions } from "@/lib/authOptions";
 import NextAuth from "next-auth/next";
 
 const handler = NextAuth(authOptions);
-
+``;
 export { handler as GET, handler as POST };
 
